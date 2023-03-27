c72358e92e34b651acf2ab0bf4da6355096caa0d
Marvin Frohwitter <marvin.frohwitter@web.de>
2023-03-27 03:56:37 +0200

    st-w3m-0.8.3.diff

--- x.c
+++ x.c
@@ -1878,6 +1878,8 @@ xpushtitle(void)
 int
 xstartdraw(void)
 {
+	if (IS_SET(MODE_VISIBLE))
+		XCopyArea(xw.dpy, xw.win, xw.buf, dc.gc, 0, 0, win.w, win.h, 0, 0);
 	return IS_SET(MODE_VISIBLE);
 }
 
