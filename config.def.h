4e59fa7ff055d86c8b93f13eea328c23f1fa2817
Marvin Frohwitter <marvin.frohwitter@web.de>
2024-05-26 15:56:18 +0200

    Added Patches to README

--- config.def.h
+++ config.def.h
@@ -5,7 +5,8 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "JetBrainsMono Nerd Font:pixelsize=14:antialias=true:autohint=true";
+// static char *font = "JetBrainsMono Nerd Font:pixelsize=14:antialias=true:autohint=true";
+static char *font = "Maple Mono:pixelsize=14:antialias=true:autohint=true";
 /* Spare fonts */
 static char *font2[] = { "JoyPixels:pixelsize=12:antialias=true:autohint=true" };
 
